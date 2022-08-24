a81c058be89b88784fb206dc28311b771130e447
FAngelo94 <a.falci@live.it>
2022-08-24 14:12:26 +0100

    added comment

--- django-todo-react/backend/todo/forms.py
+++ django-todo-react/backend/todo/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+'''
+I can use Form for an API if I don't want connect a sql table to
+an API
+'''
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
